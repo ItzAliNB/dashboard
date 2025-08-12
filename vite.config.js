a9398c9f2388436ffd058160c377bd5c1d216d35
Ali NB <aliakbarnb0903@gmail.com>
2025-08-12 16:38:39 +0330

    update

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '',
+const repoName = 'dashboard';
+
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '' : `/${repoName}/`,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+}));
